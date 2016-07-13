4caba1ff67a991316ba53d08e0cad9580c626659
ThangNguyen15 <nguyentaithang@gmail.com>
2016-07-13 21:26:23 +0700

    Java Begin Day 7

---
+++ Day 7/src/Main.java
@@ -0,0 +1,94 @@
+import java.util.Scanner;
+
+/**
+ * Created by ThangNguyen on 7/13/2016.
+ */
+public class Main {
+    public static PhoneBook phoneBook = new PhoneBook();
+    public static PhoneEntry entry = new PhoneEntry();
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int option;
+        do {
+            System.out.println("PHONE BOOK PROGRAM");
+            System.out.println("1.Add a contact");
+            System.out.println("2.Modify a contact");
+            System.out.println("3.Delete a contact");
+            System.out.println("4.Search contact");
+            System.out.println("5.Show all contact");
+            System.out.println("6.Quit program");
+            System.out.println("Enter your option: ");
+            option = input.nextInt();
+            switch (option) {
+                case 1:
+                    input.nextLine();
+                    System.out.println("Enter contact name: ");
+                    entry.setName(input.nextLine());
+                    System.out.println("Enter contact phone number: ");
+                    entry.setPhoneNumber(input.nextLine());
+                    phoneBook.addEntry(entry);
+                    break;
+                case 2:
+                    System.out.println("1. Modify contact name");
+                    System.out.println("2. Modify contact phone number");
+                    System.out.println("Enter your option: ");
+                    int choice = input.nextInt();
+                    switch (choice) {
+                        case 1:
+                            input.nextLine();
+                            System.out.println("Enter contact name you want to modify: ");
+                            String oldName = input.nextLine();
+                            System.out.println("Enter contact new name: ");
+                            String newName = input.nextLine();
+                            phoneBook.modifyEntryName(oldName, newName);
+                            break;
+                        case 2:
+                            input.nextLine();
+                            System.out.println("Enter contact name you want to modify: ");
+                            String entryName = input.nextLine();
+                            System.out.println("Enter contact new number: ");
+                            String newNumber = input.nextLine() ;
+                            phoneBook.modifyEntryNumber(entryName, newNumber);
+                            break;
+                    }
+                    break;
+                case 3:
+                    input.nextLine();
+                    System.out.println("Enter contact you want to delete: ");
+                    String entryName = input.nextLine();
+                    phoneBook.deleteEntry(entryName);
+                    break;
+                case 4:
+                    System.out.println("1. Search contact by name");
+                    System.out.println("2. Search contact by phone number");
+                    System.out.println("Enter your option: ");
+                    int opt = input.nextInt();
+                    switch (opt) {
+                        case 1:
+                            System.out.println("Enter keyword to search: ");
+                            entryName = input.nextLine();
+                            input.nextLine();
+                            phoneBook.findEntryByName(entryName);
+                            break;
+                        case 2:
+                            System.out.println("Enter number to search: ");
+                            String entryNumber = input.nextLine();
+                            input.nextLine();
+                            phoneBook.findEntryByNumber(entryNumber);
+                            break;
+                    }
+                    break;
+                case 5:
+                    System.out.println("Name\tPhone Number");
+                    if (phoneBook.getNumberOfEntries() != 0) {
+                        System.out.println(phoneBook.toString());
+                    }
+                    break;
+                case 6:
+                    break;
+            }
+        } while (option != 6);
+    }
+}
---
+++ Day 7/src/PhoneBook.java
@@ -0,0 +1,106 @@
+/**
+ * Created by ThangNguyen on 7/11/2016.
+ */
+public class PhoneBook {
+    private int numberOfEntries;
+    private PhoneEntry[] phoneEntries = new PhoneEntry[1000];
+
+    public PhoneBook() {
+    }
+
+    public int getNumberOfEntries() {
+        return numberOfEntries;
+    }
+
+    public boolean addEntry(PhoneEntry entry) {
+        if (!isDuplicateName(entry.getName()) && !isDuplicateNumber(entry.getPhoneNumber()) && numberOfEntries < phoneEntries.length) {
+            phoneEntries[numberOfEntries] = entry;
+            numberOfEntries++;
+            return true;
+        }
+        return false;
+    }
+
+    public boolean modifyEntryName(String oldName, String newName) {
+        for (int i = 0; i < numberOfEntries; i++) {
+            if (phoneEntries[i].getName().equals(oldName)) {
+                phoneEntries[i].setName(newName);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean modifyEntryNumber(String entryName, String newNumber) {
+        for (int i = 0; i < numberOfEntries; i++) {
+            if (phoneEntries[i].getName().equals(entryName)) {
+                phoneEntries[i].setPhoneNumber(newNumber);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean deleteEntry(String entryName) {
+        int index = 0;
+        if (isDuplicateName(entryName)) {
+            for (int i = 0; i < numberOfEntries; i++) {
+                if (phoneEntries[i].getName().equals(entryName)) {
+                    index = i;
+                }
+            }
+            for (int i = index + 1; i < numberOfEntries; i++) {
+                phoneEntries[i - 1] = phoneEntries[i];
+            }
+            numberOfEntries--;
+            return true;
+        }
+        return false;
+    }
+
+    public PhoneEntry findEntryByName(String entryName) {
+        for (int i = 0; i < numberOfEntries; i++) {
+            if (phoneEntries[i].getName().equals(entryName)) {
+                return phoneEntries[i];
+            }
+        }
+        return null;
+    }
+
+    public PhoneEntry findEntryByNumber(String entryNumber) {
+        for (int i = 0; i < numberOfEntries; i++) {
+            if (phoneEntries[i].getPhoneNumber().equals(entryNumber)) {
+                return phoneEntries[i];
+            }
+        }
+        return null;
+    }
+
+    public String toString() {
+        for (int i = 0; i < numberOfEntries; i++) {
+            return phoneEntries[i].toString();
+        }
+        return phoneEntries.toString();
+    }
+
+
+    private boolean isDuplicateName(String entryName) {
+        for (int i = 0; i < numberOfEntries; i++) {
+            if (phoneEntries[i].getName().equals(entryName)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean isDuplicateNumber(String entryNumber) {
+        for (int i = 0; i < numberOfEntries; i++) {
+            if (phoneEntries[i].getPhoneNumber().equals(entryNumber)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+
+}
